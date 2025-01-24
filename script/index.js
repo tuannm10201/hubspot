88018ed91d92b242a35c74244e665bb879b48d54
tuanzen8 <tuan.nguyen@zen8labs.com>
2025-01-24 13:59:34 +0700

    header complete

--- script/index.js
+++ script/index.js
@@ -1,8 +1,76 @@
+// handle slider
+const MAX_VALUE = 200000;
+
+let step = 10;
+
+function updateStep() {
+  const width = window.innerWidth;
+  if (width < 990) {
+    step = 8;
+  } else {
+    step = 10;
+  }
+  if (width < 525) {
+    step = 4;
+  } else if (width < 768) {
+    step = 5;
+  }
+  sliderEl4.max = MAX_VALUE + MAX_VALUE / step;
+  renderNode();
+}
+const tick1 = document.querySelector(".tick-1");
+const tick2 = document.querySelector(".tick-2");
+function renderNode() {
+  // render span inside tick 1
+  tick1.innerHTML = "";
+
+  const firstSpanTick1 = document.createElement("span");
+  firstSpanTick1.className = "slidertick opacity-0";
+  tick1.appendChild(firstSpanTick1);
+
+  for (let i = 0; i < step; i++) {
+    const span = document.createElement("span");
+    span.className = "slidertick";
+    tick1.appendChild(span);
+  }
+
+  const lastSpanTick1 = document.createElement("span");
+  lastSpanTick1.className = "slidertick position-relative";
+  lastSpanTick1.style.left = "5px";
+  tick1.appendChild(lastSpanTick1);
+
+  // render span inside tick 2
+  tick2.innerHTML = "";
+
+  const firstSpanTick2 = document.createElement("span");
+  firstSpanTick2.className = "slidertick";
+  firstSpanTick2.dataset.value = "0";
+  tick2.appendChild(firstSpanTick2);
+
+  for (let i = 0; i < step; i++) {
+    const span = document.createElement("span");
+    span.className = "slidertick";
+    const divDataSet = document.createElement("div");
+    const value = ((i + 1) * MAX_VALUE) / step;
+    divDataSet.dataset.value = value;
+    divDataSet.innerText = formatCurrency(value);
+    span.appendChild(divDataSet);
+    tick2.appendChild(span);
+  }
+
+  const lastSpanTick2 = document.createElement("span");
+  lastSpanTick2.className = "slidertick";
+  const divDataSet = document.createElement("div");
+  divDataSet.dataset.value = sliderEl4.max;
+  divDataSet.innerText = "Talk to sales";
+  lastSpanTick2.appendChild(divDataSet);
+  tick2.appendChild(lastSpanTick2);
+
+  addEventSliderTicks2();
+}
+
 const sliderEl4 = document.querySelector("#range4");
 const sliderValue4 = document.querySelector(".value4");
-
-const sliderTicks2 = document.querySelectorAll(".tick-2 span");
-const sliderTicks1 = document.querySelectorAll(".tick-1 span");
 
 const inputColor = document.querySelector(".input-color");
 
@@ -10,27 +78,36 @@ const labelValueSlide = document.querySelector(
   ".estimation-range .off-white-btn"
 );
 sliderEl4.addEventListener("input", (event) => {
-  const indexValue = event.target.value;
-  hideTickNode(indexValue == 0 ? 1 : indexValue);
+  let indexValue = event.target.value;
+  if (indexValue > MAX_VALUE) {
+    indexValue = sliderEl4.max;
+  }
+  hideTickNode(indexValue);
 });
+function addEventSliderTicks2() {
+  const sliderTicks2 = document.querySelectorAll(".tick-2 span");
 
-sliderTicks2.forEach((tick) => {
-  tick.addEventListener("click", (event) => {
-    const indexValue = event.target.dataset.value;
-
-    hideTickNode(indexValue);
+  sliderTicks2.forEach((tick) => {
+    tick.addEventListener("click", (event) => {
+      const indexValue = event.target.dataset.value;
+      hideTickNode(indexValue);
+    });
   });
-});
-
+}
 function hideTickNode(indexValue) {
+  indexValue = Math.max(10000, +indexValue);
   const progress = (indexValue / sliderEl4.max) * 100;
   sliderEl4.value = indexValue;
   updatePrice(indexValue);
   sliderEl4.style.background = ` linear-gradient(90deg, #BB1EF2 0%, #3D5DFF ${progress}%)`;
   inputColor.style.width = `calc(${100 - progress}%)`;
+  const sliderTicks1 = document.querySelectorAll(".tick-1 span");
   sliderTicks1.forEach((tick, index) => {
     tick.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
-    if (index === +indexValue) {
+    const stepShow = MAX_VALUE / step;
+    const shouldHideNode =
+      Math.abs(index * stepShow - indexValue) < MAX_VALUE / 100;
+    if (shouldHideNode) {
       tick.style.backgroundColor = "unset";
     }
   });
@@ -41,31 +118,35 @@ const corePrice = document.querySelector("#core-price");
 const BASE_STARTER_PRICE = 2400;
 const BASE_CORE_PRICE = 4100;
 const monthBilleds = document.querySelectorAll(".month-billed");
+const deliver = document.querySelector("#deliver");
 
 function updatePrice(indexValue) {
-  if (indexValue == 12) {
+  if (indexValue > MAX_VALUE) {
     starterPrice.textContent = "Custom";
     corePrice.textContent = "Custom";
+    deliver.textContent = "Custom";
 
     monthBilleds.forEach((monthBilled) => {
       monthBilled.classList.add("d-none");
     });
   } else {
-    const starterPriceValue = BASE_STARTER_PRICE + +indexValue - 1;
-    const corePriceValue = BASE_CORE_PRICE + +indexValue - 1;
-    starterPrice.textContent = formatCurrency(starterPriceValue);
-    corePrice.textContent = formatCurrency(corePriceValue);
+    const starterPriceValue = BASE_STARTER_PRICE + +indexValue;
+    const corePriceValue = BASE_CORE_PRICE + +indexValue;
+    starterPrice.textContent = "$" + formatCurrency(starterPriceValue);
+    corePrice.textContent = "$" + formatCurrency(corePriceValue);
+    deliver.textContent = formatCurrency(indexValue);
 
     monthBilleds.forEach((monthBilled) => {
       monthBilled.classList.remove("d-none");
     });
   }
 }
-hideTickNode(1);
+hideTickNode(10000);
 
 function formatCurrency(number) {
-  return "$" + number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+// end handle slider
 
 const header = document.querySelector("header");
 let lastScrollTop = 0;
@@ -115,4 +196,9 @@ function setEqualHeight() {
 
 // Run when the window is ready and on resize
 setEqualHeight();
-window.addEventListener("resize", setEqualHeight);
+updateStep();
+window.addEventListener("resize", () => {
+  setEqualHeight();
+  updateStep();
+  //update step base on width
+});
